898ae23fe4391708e0cb9696926da06d874b21fb
ChangyeonYoo <devwgod@gmail.com>
2021-01-21 04:35:09 +0900

    과제 목록 validation

--- src/main/java/in/hangang/domain/Assignment.java
+++ src/main/java/in/hangang/domain/Assignment.java
@@ -1,10 +1,14 @@
 package in.hangang.domain;
 
+import in.hangang.annotation.ValidationGroups;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Assignment {
+
+    @Range(groups = {ValidationGroups.createReview.class}, min = 1, max = 4, message = "강의 정보 id는 1~4 사이의 숫자입니다.")
     Long id;
     @ApiModelProperty(hidden = true)
     String name;
