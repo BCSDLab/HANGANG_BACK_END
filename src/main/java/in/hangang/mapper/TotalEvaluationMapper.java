5b92d7e8b7ea1d733af581ea592537634348820f
ChangyeonYoo <devwgod@gmail.com>
2021-01-27 22:04:44 +0900

    강의 종합평가, 평점 분포 api 작성

--- src/main/java/in/hangang/mapper/TotalEvaluationMapper.java
+++ src/main/java/in/hangang/mapper/TotalEvaluationMapper.java
@@ -1,9 +1,17 @@
 package in.hangang.mapper;
 
+import in.hangang.domain.Review;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public interface TotalEvaluationMapper {
     float getTotalRating(Long id);
-    void updateTotalRating();
+    //void updateTotalRating();
+    Review getTotalEvaluationByLectureId(Long id);
+    ArrayList<HashMap<String, String>> getRatingCountByLectureId(Long id);
 }
