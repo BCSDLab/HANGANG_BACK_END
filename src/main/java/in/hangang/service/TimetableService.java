974aa5711cb8356b862884e4cd94d5508bf30812
ChangyeonYoo <devwgod@gmail.com>
2021-03-07 01:15:59 +0900

    시간표 CRUD, 시간표 내에 강의 CRUD

--- src/main/java/in/hangang/service/TimetableService.java
+++ src/main/java/in/hangang/service/TimetableService.java
@@ -3,16 +3,14 @@ package in.hangang.service;
 import in.hangang.domain.LectureTimeTable;
 import in.hangang.domain.TimeTable;
 import in.hangang.domain.UserTimetable;
-
-import java.sql.Time;
 import java.util.ArrayList;
 
 public interface TimetableService {
     ArrayList<UserTimetable> getTableListByUserId() throws Exception;
     void createTimetable(UserTimetable userTimetable) throws Exception;
-    void deleteTimetable(Long timeTableId) throws Exception;
+    void deleteTimetable(TimeTable timeTable) throws Exception;
     void createLectureOnTimeTable(TimeTable timeTable) throws Exception;
-    void deleteLectureOnTimeTable(TimeTable timeTable, Long lectureId) throws Exception;
+    void deleteLectureOnTimeTable(TimeTable timeTable) throws Exception;
     ArrayList<LectureTimeTable> getLectureListByTimeTableId(Long timeTableId) throws Exception;
 
 
