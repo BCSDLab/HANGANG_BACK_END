129e0547f561bc6762c93f7f3320634d6edd39bb
tngus3722 <tngus3722@koreatech.ac.kr>
2021-05-18 11:02:08 +0900

    포인트제도 수정

--- src/main/java/in/hangang/enums/Point.java
+++ src/main/java/in/hangang/enums/Point.java
@@ -3,10 +3,10 @@ package in.hangang.enums;
 import org.springframework.http.HttpStatus;
 
 public enum Point {
-    SIGN_UP("유저 회원가입", 20,1),
+    SIGN_UP("유저 회원가입", 300,1),
     LECTURE_BANK("강의자료 구매",100, 2),
-    LECTURE_REVIEW("강의평 작성", 20,3),
-    LECTURE_UPLOAD("강의자료 업로드", 50,4);
+    LECTURE_REVIEW("강의평 작성", 30,3),
+    LECTURE_UPLOAD("강의자료 업로드", 70,4);
 
 
     private String name;
